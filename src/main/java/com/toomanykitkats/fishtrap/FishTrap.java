b9452ce12f6f49c14264b137934b67e4e206e6d9
TooManyKitKats <Bosley2004@icloud.com>
2022-12-12 22:19:44 -0500

    fix

--- src/main/java/com/toomanykitkats/fishtrap/FishTrap.java
+++ src/main/java/com/toomanykitkats/fishtrap/FishTrap.java
@@ -1,7 +1,9 @@
 package com.toomanykitkats.fishtrap;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod("fishtrap")
 public class FishTrap {
     public FishTrap() {
         MinecraftForge.EVENT_BUS.register(this);
